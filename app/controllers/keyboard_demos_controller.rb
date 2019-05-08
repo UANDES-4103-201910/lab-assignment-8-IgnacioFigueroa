3e6103c2aa232f58c6bb4356a41ee2f7fd78d13b
ignaciofigueroa <ijfigueroa@miuandes.cl>
2019-05-08 07:26:40 -0400

    im done

--- app/controllers/keyboard_demos_controller.rb
+++ app/controllers/keyboard_demos_controller.rb
@@ -1,4 +1,7 @@
 class KeyboardDemosController < ApplicationController
   def demo1
   end
+  def demo2
+    render("keyboard_demos/demo2")
+  end
 end
---
+++ public/assets/js/keyboard_od.js
@@ -0,0 +1,33 @@
+// Note: $(() => {}); is equivalent to $(document).ready(function(){})
+class KeyboardController2{
+    constructor(inputId){
+        this.inputId = inputId;
+        this.target = document.getElementById(inputId)
+    }
+
+    write(key){
+        console.log(key.textContent);
+        if (key.textContent.includes("Enter")){
+            this.target.append("<br>");
+        }
+        else if(key.textContent.includes("Bksp")){
+            var thisText2 = this.target.text();
+            var newText2 = thisText2.substr(0,thisText2.length-1*3);
+            this.target.text(newText2);
+        }
+        else {
+            this.target.append(key.textContent.replace(/ /g,''));
+        }
+    }
+}
+
+
+$(() => {
+    var keyboardController = new KeyboardController2(document.getElementById("keyboard-input").id);
+	$(".key").click(function (e) {
+        keyboardController.write(this);
+    });
+    $("#toggler").click(function(e){
+        $(".keyboard_row").toggle();
+    })
+});
